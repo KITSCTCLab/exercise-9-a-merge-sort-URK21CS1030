f808920bd3e1178afc18fdfd29adc152d8829b3b
URK21CS1030 <109951884+URK21CS1030@users.noreply.github.com>
2022-10-10 14:50:02 +0530

    initial commit 1

--- Main.py
+++ Main.py
@@ -2,6 +2,36 @@ from typing import List
 
 def merge_sort(data) -> None:
   # Write code here
+  if len(data) > 1:
+    mid = len(data)//2
+    left_array = data[:mid]
+    right_array = data[mid:]
+
+    merge_sort(left_array)
+    merge_sort(right_array)
+
+    i=0
+    j=0
+    k=0
+
+    while i < len(left_array) and j < len(right_array):
+      if left_array[i] <= right_array[j]:
+        data[k] = left_array[i]
+        i += 1
+      else:
+        data[k] = right_array[j]
+        j += 1
+      k += 1
+
+    while i < len(left_array):
+      data[k] = left_array[i]
+      i += 1
+      k += 1
+
+    while j < len(right_array):
+       data[k]=right_array[j]
+       j += 1
+       k += 1
 
 
 # Do not change the following code
